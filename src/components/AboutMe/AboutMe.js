98b2a71052516492a12204e464a069fe85073424
Elisaveta Vlad <vladelisabeta@yandex.ru>
2023-12-10 01:17:43 +0300

    minor fixes

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -11,7 +11,7 @@ function AboutMe() {
                         <h3 className='about-me__title-name'>Елизавета</h3>
                         <p className='about-me__occupation'>Студент Я.Практикум</p>
                         <p className='about-me__text'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
-                        <a href='/' className='about-me__github'>Github</a>
+                        <a href='https://github.com/vladelisabeta' className='about-me__github' target="_blank" rel="noopener noreferrer">Github</a>
                     </div>
                     <img src={avatar} alt='аватар студента' className='about-me__avatar' />
                 </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,8 +7,8 @@ function Footer() {
                 <p className='footer__text'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
                 <div className='footer__container'>
                     <div className='footer__link-box'>
-                        <a href='/' className='footer__link'>Яндекс.Практикум</a>
-                        <a href='/' className='footer__link'>Github</a>
+                        <a href='https://practicum.yandex.ru/' className='footer__link' target="_blank" rel="noopener noreferrer">Яндекс.Практикум</a>
+                        <a href='https://github.com/vladelisabeta' className='footer__link' target="_blank" rel="noopener noreferrer">Github</a>
                     </div>
                     <p className='footer__copyright'>vladelisabeta&copy;2023</p>
                 </div>
